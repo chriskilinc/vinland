4a780ff0afff399e21c3b21a1d6405efd3dcda6b
Christopher Kilinc <chriskilinc@gmail.com>
2025-03-04 21:29:54 +0100

    Render Tiles and Selection (WIP)

--- Tile.cs
+++ Tile.cs
@@ -21,23 +21,107 @@ namespace Vinland
         Floor
     }
 
-    public partial class Tile
+    public partial class Tile : Node3D
     {
+        static StringName _inputMouseLeft = new StringName("mouse_left");
+
+        [Export]
         public TileType Type { get; set; }
 
         Item item;
         StaticObject staticObject;
 
         World world;
-        int x;
-        int y;
 
-        public Tile(World world, int x, int y)
+        [Export]
+        public int x;
+
+        [Export]
+        public int y;
+
+        StandardMaterial3D OriginalMaterial;
+
+        private MeshInstance3D meshInstance;
+
+        // Static property to keep track of the selected tile
+        private static Tile selectedTile;
+
+        public override void _Ready()
+        {
+
+        }
+
+        public Tile Init(World world, int x, int y)
         {
             Type = TileType.Empty;
             this.world = world;
             this.x = x;
             this.y = y;
+
+            Position = new Vector3(x, 0, y);
+            Name = "Tile " + x + ", " + y;
+
+            meshInstance = GetNode<MeshInstance3D>("MeshInstance3D");
+            if (meshInstance != null)
+            {
+                // meshInstance.Position = new Vector3(x, 0, y);
+
+                StandardMaterial3D material = new StandardMaterial3D();
+                // material.AlbedoColor = new Color(1, 1, 1);
+
+                Random random = new Random();
+                Color randomColor = new Color(
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble(),
+                    (float)random.NextDouble()
+                );
+
+                material.AlbedoColor = randomColor;
+                meshInstance.MaterialOverride = material;
+
+                OriginalMaterial = material;
+            }
+
+            return this;
+        }
+
+        public void OnInputEvent(Node camera, InputEvent @event, Vector3 click_position, Vector3 click_normal, int shape_idx)
+        {
+            if (@event is InputEventMouseButton mouseButton)
+            {
+                if (mouseButton.IsActionPressed(_inputMouseLeft))
+                {
+                    GD.Print("click on tile ", x, ", ", y);
+
+                    // TODO: Deselect the previously selected tile
+                    if (selectedTile != null && selectedTile != this)
+                    {
+                        selectedTile.Deselect();
+                    }
+
+                    // Select the current tile
+                    Select();
+                }
+            }
+        }
+
+        private void Select()
+        {
+            selectedTile = this;
+            if (meshInstance != null)
+            {
+                StandardMaterial3D material = new StandardMaterial3D();
+                material.AlbedoColor = new Color(1, 0, 0); // Red color to indicate selection
+                meshInstance.MaterialOverride = material;
+            }
+        }
+
+        private void Deselect()
+        {
+            if (meshInstance != null)
+            {
+                meshInstance.MaterialOverride = OriginalMaterial; // Revert to the original material
+            }
         }
     }
 }
--- World.cs
+++ World.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Vinland
 {
-    public partial class World : Node
+    public partial class World : Node3D
     {
         private Tile[,] tiles;
         int width = 100;
@@ -12,20 +12,47 @@ namespace Vinland
         public World()
         {
             GD.Print("Creating world");
+            var TileScene = GD.Load<PackedScene>("res://tile.tscn");
 
-            tiles = new Tile[width, height];
+
+            // tiles = new Tile[width, height];
 
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
-                {
-                    tiles[x, y] = new Tile(this, x, y);
+                {   
+                    var a = TileScene.Instantiate<Tile>().Init(this, x, y);
+                    AddChild(a);
+                    // tiles[x, y] = new Tile(this, x, y);
                 }
             }
 
             GD.Print("World created");
-            GD.Print("Tiles: ", tiles.Length);
-            GD.Print("Origen: ", tiles[0, 0]);
+            // GD.Print("Tiles: ", tiles.Length);
+            // GD.Print("Origen: ", tiles[0, 0]);
+
+
+            // spawn tiles in world
+            // foreach (Tile tile in tiles)
+            // {
+            //     // TileScene into world
+            //     // TileScene.Instantiate<Tile>();
+
+
+            //     // tile.Position = new Vector3(tile.x, 0, tile.y);
+            //     // AddChild(tile);
+            // }
+        }
+
+        public override void _Ready()
+        {
+            GD.Print("World ready");
+        }
+
+        public override void _Process(double delta)
+        {
+            // Called every frame. Delta is the elapsed time since the previous frame.
+            // Update game logic here.
         }
 
         public Tile GetTileAt(int x, int y)
